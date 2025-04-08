b868b2964545eae744ceed5349faa7a649e5df12
Marta <marta_rodrigolloveras@iescarlesvallbona.cat>
2025-04-08 17:00:38 +0200

    Refactor auth service to include token authorization in logout and getUserProfile methods

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,10 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../environments/environment';
 import { AuthCredentials } from '../models/auth-credentials.model';
 import { UserRegister } from '../models/user-register.model';
+import { TokenService } from './token.service';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +12,7 @@ import { UserRegister } from '../models/user-register.model';
 export class AuthService {
   private readonly API_URL = environment.apiURL;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private tokenService: TokenService) {}
 
   login(credentials: AuthCredentials): Observable<any> {
     return this.http.post(`${this.API_URL}/login`, credentials);
@@ -22,10 +23,16 @@ export class AuthService {
   }
 
   logout(): Observable<any> {
-    return this.http.delete(`${this.API_URL}/logout`);
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${this.tokenService.getToken()}`
+    });
+    return this.http.delete(`${this.API_URL}/logout`, { headers });
   }
 
   getUserProfile(): Observable<any> {
-    return this.http.get(`${this.API_URL}/user-profile`);
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${this.tokenService.getToken()}`
+    });
+    return this.http.get(`${this.API_URL}/user-profile`, { headers });
   }
 }
