1f8f9561bac4ee66f5a10836182a7a705b48d5e1
Eric Gómez <eric.gomez.egp@gmail.com>
2025-04-29 21:05:08 +0200

    s'afegeix la funcionalitat per redireccionar els usuaris al dashboard corresponent segons el seu tipus després de l'inici de sessió

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { TokenService } from '../services/token.service';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -9,11 +10,26 @@ import { TokenService } from '../services/token.service';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-  constructor(private tokenService: TokenService, private router: Router) {}
+  constructor(
+    private tokenService: TokenService, 
+    private router: Router,
+    private authService: AuthService
+  ) {}
 
   navigateToDashboardOrLogin(): void {
     if (this.tokenService.isLoggedIn()) {
-      this.router.navigate(['/dashboard']); 
+      // Determinar el tipo de usuario y redireccionar al dashboard correspondiente
+      this.authService.getUserType().subscribe(userType => {
+        if (userType === 'protectora') {
+          this.router.navigate(['/dashboard']);
+        } else {
+          this.router.navigate(['/user-dashboard']);
+        }
+      }, error => {
+        console.error('Error al obtener el tipo de usuario:', error);
+        // Si hay un error, redirigir al dashboard general
+        this.router.navigate(['/dashboard']);
+      });
     } else {
       this.router.navigate(['/login']); 
     }
