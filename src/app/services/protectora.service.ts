5971181daa11da009977bacdc9e819677b97b979
Marta <marta_rodrigolloveras@iescarlesvallbona.cat>
2025-05-12 15:49:18 +0200

    s'implementa un xat entre usuaris FALTA ARREGLAR PROBLEMA IMATGE PROTECTORA, ACTUALMENT ES IMPOSSIBLE

--- src/app/services/protectora.service.ts
+++ src/app/services/protectora.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { catchError, Observable, tap, throwError } from 'rxjs';
 import { Protectora } from '../interfaces/protectora';
 
 @Injectable({
@@ -18,8 +18,14 @@ export class ProtectoraService {
     return this.http.get<Protectora>(`${this.apiUrl}/protectora/get/${id}`);
   }
   getProtectoraByUsuario(usuarioId: number): Observable<Protectora> {
-    console.log('Solicitando información de la protectora para el usuario con ID:', usuarioId);
-    return this.http.get<Protectora>(`${this.apiUrl}/protectora/usuario/${usuarioId}`);
+    console.log('Llamando al endpoint /api/protectora/usuario con ID:', usuarioId);
+    return this.http.get<Protectora>(`http://127.0.0.1:8000/api/protectora/usuario/${usuarioId}`).pipe(
+      tap((response) => console.log('Respuesta del endpoint /api/protectora/usuario:', response)),
+      catchError((error) => {
+        console.error('Error al obtener la protectora:', error);
+        return throwError(() => new Error('No se pudo obtener la protectora.'));
+      })
+    );
   }
   createProtectora(protectoraData: FormData): Observable<Protectora> {
     return this.http.post<Protectora>(`${this.apiUrl}/protectora/create`, protectoraData);
